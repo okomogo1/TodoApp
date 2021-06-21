7b8090c22780fc6ae47b3d86fe14764e762c94de
okomogo1 <okomogo1@gmail.com>
2021-06-21 18:04:13 +0100

    FirstLoginApp

--- app/src/main/java/com/timothy/todoapp/MainActivity.kt
+++ app/src/main/java/com/timothy/todoapp/MainActivity.kt
@@ -2,8 +2,12 @@ package com.timothy.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    val username ="admin"
+    val password = "admin"
+    private lateinit var usernameinput: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
